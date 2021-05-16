678e44894ec5b19e4a993ab226da966101c64577
alexvmv <alexvmv@outlook.com>
2021-05-16 16:05:37 +0300

    Commit

--- src/Test4/Test4.java
+++ src/Test4/Test4.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Test4 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите значение x:");
         float x = (float) scan.nextDouble();
