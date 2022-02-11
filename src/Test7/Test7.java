8abe295800a3989a497698567d50a7c7e553930b
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2022-02-11 20:31:08 +0300

    test

---
+++ src/Test7/Test7.java
@@ -0,0 +1,13 @@
+package Test7;
+
+import java.util.Scanner;
+
+public class Test7 {
+    public static void main(String[] args) {
+        int length;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите длину массива");
+        length = scan.nextInt();
+    }
+}
+
