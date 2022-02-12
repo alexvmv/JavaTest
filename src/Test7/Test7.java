27dcddf4fc61f8f74c842514e5981faeebcab303
alexvmv <alexvmv@outlook.com>
2022-02-12 13:35:44 +0300

    Commit

--- src/Test7/Test7.java
+++ src/Test7/Test7.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Test7 {
     public static void main(String[] args) {
         int length;
+        final int x = 20;
+        final int y = 3;
+        final int z = 8;
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите длину массива");
         length = scan.nextInt();
+        int[] myArray = new int[length];
+        for (int i=0; i<length; i++){
+            System.out.println("Введите элемент массива:");
+            myArray[i] = scan.nextInt();
+            if (myArray[i]==x | myArray[i]==y | myArray[i]==z)
+                System.out.println("Данное значение имеется в константах");
+        }
     }
 }
 
