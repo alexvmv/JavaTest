a9f9639939fbb9b072c73e746c47f21c1d95496c
alexvmv <alexvmv@outlook.com>
2022-02-12 22:36:40 +0300

    Commit

--- src/Test9/Test9.java
+++ src/Test9/Test9.java
@@ -11,6 +11,7 @@ public class Test9 {
         System.out.println("Введите размер массива");
         length = scan.nextInt();
         double[] myArray = new double[length];
+
         for (int i=0; i<length; i++) {
             System.out.println("Введите элемент массива:");
             myArray[i] = scan.nextDouble();
@@ -22,6 +23,7 @@ public class Test9 {
 
         avg= sum/ myArray.length;
         System.out.println("Cреднее арифметическое элементов массива = "+avg);
+
         for (double x: myArray) {
             x*=avg;
             System.out.println(x);
