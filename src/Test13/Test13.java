6b661edb5a3a8228aff2dcab7119351fb09414de
alexvmv <alexvmv@outlook.com>
2022-02-17 22:16:28 +0300

    Commit

--- src/Test13/Test13.java
+++ src/Test13/Test13.java
@@ -8,7 +8,15 @@ public class Test13 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите строку:");
         str = scan.nextLine();
-
-
+        int count = 0;
+        String[] words = str.split(" ");
+        for (String mystr : words) {
+            if (mystr.matches("^[a-zA-Z]+$")) {
+                System.out.print(mystr + " ");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("Количество слов "+ count);
     }
 }
